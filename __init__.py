2a20f9bf1ff8303df94128e37336a2c8366f7a88
Grant Gapinski <grant-gapinski@bethel.edu>
2017-06-06 12:27:07 -0500

    Deleting Files / Refactoring
    Removed unused files
    Starting refactoring to fit AJAX into one method

--- __init__.py
+++ __init__.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route("/buttons")
 def buttons():
-    my_list = []
-    for x in range(0, 100):
-        my_list.append(x)
+    return render_template('index.html')
 
-    return render_template('index.html', **locals())
-
-@app.route("/get_red_color")
-def get_red_color():
-    return "red"
+@app.route("/get_color")
+def get_color(passIn):
+    color = passIn
+    return color
