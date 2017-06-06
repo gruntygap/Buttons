1db6e2997b53b7fd35a0e5a708073be54cd81cab
Grant Gapinski <grant-gapinski@bethel.edu>
2017-06-06 15:29:10 -0500

    Completed "part 2"
    Part 2 simply changes to a random CSS color once clicked.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 app = Flask(__name__)
 
 
@@ -15,12 +16,39 @@ def buttons_v2():
 @app.route("/get_color")
 def get_color():
     pass_in = request.args
-    if pass_in.get("key", "null") == "red":
+    if pass_in.get("key", "null") == 'red':
         color = 'tomato'
-    elif pass_in.get("key", "null") == "green":
+    elif pass_in.get("key", "null") == 'green':
         color = 'forestgreen'
     elif pass_in.get("key", "null") == 'blue':
         color = 'aqua'
     else:
         color = 'No Color fit the parameter'
     return color
+
+
+@app.route("/get_rand_color")
+def get_rand_color():
+    css_color_names = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black",
+                       "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse",
+                       "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan",
+                       "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta",
+                       "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen",
+                       "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink",
+                       "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen",
+                       "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green",
+                       "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender",
+                       "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan",
+                       "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon",
+                       "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue",
+                       "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine",
+                       "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue",
+                       "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream",
+                       "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange",
+                       "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed",
+                       "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown",
+                       "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver",
+                       "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan",
+                       "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow",
+                       "YellowGreen"]
+    return css_color_names[random.randint(0,140)]
