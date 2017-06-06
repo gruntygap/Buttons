6e896d461e2b105ba7410c5ff5e6996cbfc73012
Grant Gapinski <grant-gapinski@bethel.edu>
2017-06-06 12:10:17 -0500

    Retiring Branch
    Moving on with issues to Buttons V2

--- run.py
+++ run.py
@@ -1,15 +1,3 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from __init__ import app
 
-
-@app.route("/")
-def template_test():
-    my_list = []
-    for x in range(0, 100):
-        my_list.append(x)
-
-    return render_template('index.html', my_string="Wheeeee!", my_list = my_list)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True, port=5000)
